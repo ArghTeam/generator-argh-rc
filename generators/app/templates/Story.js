08c4402cd450ab22b48a85c87419ade5e4a53e60
ftroitsky <fedor.troitsky@gmail.com>
2019-05-12 16:28:04 +0300

    Keep stories in component folder

--- generators/app/templates/Story.js
+++ generators/app/templates/Story.js
@@ -4,7 +4,7 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
 
-import <%= name %> from '../components/<%= name %>'
+import <%= name %> from './<%= name %>'
 
 
 storiesOf('<%= name %>', module)
